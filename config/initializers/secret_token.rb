2e625a08e8252a9f5cab44873e2c657f52b736b4
gesarlhamo <gesarlhamo80@gmail.com>
2013-04-24 13:34:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrushMatchingApp::Application.config.secret_token = '673208e45f0ccd11e03478e45d421bb8b556770c68ed347f6bacc742571a1aef97d02de93e8b63d80084569bc0c717d48799960ea27c0e28bbb0870e3c36ad6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CrushMatchingApp::Application.config.session_store :cookie_store, key: '_CrushMatchingApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CrushMatchingApp::Application.config.session_store :active_record_store
