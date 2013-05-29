95bd9b09fa0abf3535c5eca76d97e214ad1eee8d
gesarlhamo <gesarlhamo80@gmail.com>
2013-05-28 22:25:40 -0500

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ CrushMatchingApp::Application.routes.draw do
 
   match "/home" => "static_pages#home"
 
-  get "/help" => "static_pages#help"
+  get "/about" => "static_pages#about"
 
   get "/contact" =>"static_pages#contact"
+
+  get "/help" => "static_pages#help"
 
 
   resources :targets
