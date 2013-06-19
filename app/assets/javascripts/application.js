b6424ca0ff513ff9db742c3dfcc42705111d79bf
gesarlhamo <gesarlhamo80@gmail.com>
2013-06-18 21:23:57 -0500

    used jquery to slide and toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,36 @@
+$(document).ready(function() {
+   $('button'.on('click', function(){ 
+
+
+   }); 
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
+
+
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
